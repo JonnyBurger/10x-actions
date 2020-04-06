f5d7bdfa7f9f0354d1227a60f416625ccf12d70f
Jonny Burger <jonathanburger11@gmail.com>
2020-04-06 20:08:33 +0200

    new v

--- index.js
+++ index.js
@@ -1,81 +1,158 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const exec = require("@actions/exec");
-const fs = require("fs");
-const xns = require("xns");
-
-xns(async () => {
-  const myToken = core.getInput("github-token");
-  const cwd = core.getInput("pod-dir");
-
-  const octokit = new github.GitHub(myToken);
-
-  const ref = await octokit.git.getRef(
-    github.context.repo.owner,
-    github.context.repo.repo,
-    github.context.ref
-  );
-  const latestCommit = await octokit.git.getCommit(
-    github.context.repo.owner,
-    github.context.repo.repo,
-    ref.data.object.sha
-  );
-  const podfileBefore = await fs.promises.readFile(`${cwd}/Podfile`, "utf-8");
-  const podfileLockBefore = await fs.promises.readFile(
-    `${cwd}/Podfile.lock`,
-    "utf-8"
-  );
-  const podfilePath = `${cwd}/Podfile`;
-  const podfileLockPath = `${cwd}/Podfile.lock`;
-  console.log("Got Podfile before, now running pod install...");
-  exec.exec("pod", ["install"], {
-    cwd,
-  });
-  const podfileAfter = await fs.promises.readFile(podfilePath, "utf-8");
-  const podfileLockAfter = await fs.promises.readFile(podfileLockPath, "utf-8");
-  console.log("Got Podfile before, now running pod install...");
-  if (podfileAfter !== podfileLockAfter || podfileBefore !== podfileAfter) {
-    console.log("The Podfile is different, let me fix that");
-    const podfileBlob = await octokit.git.createBlob({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      body: podfileAfter,
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-    const commit = await octokit.git.createTree({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      base_tree: latestCommit.data.sha,
-      tree: [
-        {
-          content: podfileAfter,
-          path: podfilePath,
-          sha: podfileBlob.data.sha,
-          type: "blob",
-          mode: "100644",
-        },
-      ],
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = __importDefault(require("@actions/core"));
+var github_1 = __importDefault(require("@actions/github"));
+var exec_1 = __importDefault(require("@actions/exec"));
+var fs_1 = __importDefault(require("fs"));
+var xns_1 = require("xns");
+xns_1.xns(function () { return __awaiter(void 0, void 0, void 0, function () {
+    var myToken, cwd, octokit, _a, ref, _b, owner, repo, curentRef, latestCommit, podfileBefore, podfileLockBefore, podfilePath, podfileLockPath, podfileAfter, podfileLockAfter, podfileBlob, podfileLockBlob, commit;
+    return __generator(this, function (_c) {
+        switch (_c.label) {
+            case 0:
+                myToken = core_1.default.getInput("github-token");
+                cwd = core_1.default.getInput("pod-dir");
+                octokit = new github_1.default.GitHub(myToken);
+                _a = github_1.default.context, ref = _a.ref, _b = _a.repo, owner = _b.owner, repo = _b.repo;
+                return [4 /*yield*/, octokit.git.getRef({
+                        owner: owner,
+                        repo: repo,
+                        ref: ref,
+                    })];
+            case 1:
+                curentRef = _c.sent();
+                return [4 /*yield*/, octokit.git.getCommit({
+                        owner: owner,
+                        repo: repo,
+                        commit_sha: curentRef.data.object.sha,
+                    })];
+            case 2:
+                latestCommit = _c.sent();
+                return [4 /*yield*/, fs_1.default.promises.readFile(cwd + "/Podfile", "utf-8")];
+            case 3:
+                podfileBefore = _c.sent();
+                return [4 /*yield*/, fs_1.default.promises.readFile(cwd + "/Podfile.lock", "utf-8")];
+            case 4:
+                podfileLockBefore = _c.sent();
+                podfilePath = cwd + "/Podfile";
+                podfileLockPath = cwd + "/Podfile.lock";
+                console.log("Got Podfile before, now running pod install...");
+                exec_1.default.exec("pod", ["install"], {
+                    cwd: cwd,
+                });
+                return [4 /*yield*/, fs_1.default.promises.readFile(podfilePath, "utf-8")];
+            case 5:
+                podfileAfter = _c.sent();
+                return [4 /*yield*/, fs_1.default.promises.readFile(podfileLockPath, "utf-8")];
+            case 6:
+                podfileLockAfter = _c.sent();
+                console.log("Got Podfile before, now running pod install...");
+                if (!(podfileLockBefore !== podfileLockAfter ||
+                    podfileBefore !== podfileAfter)) return [3 /*break*/, 12];
+                console.log("The Podfile is different, let me fix that");
+                return [4 /*yield*/, octokit.git.createBlob({
+                        repo: github_1.default.context.repo.repo,
+                        owner: github_1.default.context.repo.owner,
+                        content: podfileAfter,
+                    })];
+            case 7:
+                podfileBlob = _c.sent();
+                return [4 /*yield*/, octokit.git.createBlob({
+                        repo: github_1.default.context.repo.repo,
+                        owner: github_1.default.context.repo.owner,
+                        content: podfileLockAfter,
+                    })];
+            case 8:
+                podfileLockBlob = _c.sent();
+                return [4 /*yield*/, octokit.git.createTree({
+                        repo: github_1.default.context.repo.repo,
+                        owner: github_1.default.context.repo.owner,
+                        base_tree: latestCommit.data.sha,
+                        tree: [
+                            {
+                                content: podfileAfter,
+                                path: podfilePath,
+                                sha: podfileBlob.data.sha,
+                                type: "blob",
+                                mode: "100644",
+                            },
+                            {
+                                content: podfileLockAfter,
+                                path: podfileLockPath,
+                                sha: podfileLockBlob.data.sha,
+                                type: "blob",
+                                mode: "100644",
+                            },
+                        ],
+                    })];
+            case 9:
+                commit = _c.sent();
+                return [4 /*yield*/, octokit.git.createCommit({
+                        repo: github_1.default.context.repo.repo,
+                        owner: github_1.default.context.repo.owner,
+                        committer: {
+                            email: "hi@jonny.io",
+                            name: "jonnybot",
+                        },
+                        message: "🤖 Fixed your fucking Podfile",
+                        tree: commit.data.sha,
+                        parents: [curentRef.data.object.sha],
+                    })];
+            case 10:
+                _c.sent();
+                return [4 /*yield*/, octokit.git.updateRef({
+                        repo: repo,
+                        owner: owner,
+                        ref: ref,
+                        sha: curentRef.data.object.sha,
+                    })];
+            case 11:
+                _c.sent();
+                console.log("Fixed the fucking Podfile. Failing this commit now, wait for the next one!");
+                throw new Error("Podfile is not up to date (commit fix was made)");
+            case 12:
+                console.log("Pod file is up to date!");
+                _c.label = 13;
+            case 13: return [2 /*return*/];
+        }
     });
-    await octokit.git.createCommit({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      committer: {
-        email: "hi@jonny.io",
-        name: "jonnybot",
-      },
-      message: "🤖 Fixed your fucking Podfile",
-      tree: commit.data.sha,
-      parents: [ref.data.object.sha],
-    });
-    await octokit.git.updateRef({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      ref: github.context.ref,
-    });
-    console.log(
-      "Fixed the fucking Podfile. Failing this commit now, wait for the next one!"
-    );
-    throw new Error("Podfile is not up to date (commit fix was made)");
-  } else {
-    console.log("Pod file is up to date!");
-  }
-});
+}); });
---
+++ index.ts
@@ -0,0 +1,102 @@
+import core from "@actions/core";
+import github from "@actions/github";
+import exec from "@actions/exec";
+import fs from "fs";
+import { xns } from "xns";
+
+xns(async () => {
+  const myToken = core.getInput("github-token");
+  const cwd = core.getInput("pod-dir");
+
+  const octokit = new github.GitHub(myToken);
+
+  const {
+    ref,
+    repo: { owner, repo },
+  } = github.context;
+
+  const curentRef = await octokit.git.getRef({
+    owner,
+    repo,
+    ref,
+  });
+  const latestCommit = await octokit.git.getCommit({
+    owner,
+    repo,
+    commit_sha: curentRef.data.object.sha,
+  });
+  const podfileBefore = await fs.promises.readFile(`${cwd}/Podfile`, "utf-8");
+  const podfileLockBefore = await fs.promises.readFile(
+    `${cwd}/Podfile.lock`,
+    "utf-8"
+  );
+  const podfilePath = `${cwd}/Podfile`;
+  const podfileLockPath = `${cwd}/Podfile.lock`;
+  console.log("Got Podfile before, now running pod install...");
+  exec.exec("pod", ["install"], {
+    cwd,
+  });
+  const podfileAfter = await fs.promises.readFile(podfilePath, "utf-8");
+  const podfileLockAfter = await fs.promises.readFile(podfileLockPath, "utf-8");
+  console.log("Got Podfile before, now running pod install...");
+  if (
+    podfileLockBefore !== podfileLockAfter ||
+    podfileBefore !== podfileAfter
+  ) {
+    console.log("The Podfile is different, let me fix that");
+    const podfileBlob = await octokit.git.createBlob({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      content: podfileAfter,
+    });
+    const podfileLockBlob = await octokit.git.createBlob({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      content: podfileLockAfter,
+    });
+    const commit = await octokit.git.createTree({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      base_tree: latestCommit.data.sha,
+      tree: [
+        {
+          content: podfileAfter,
+          path: podfilePath,
+          sha: podfileBlob.data.sha,
+          type: "blob",
+          mode: "100644",
+        },
+        {
+          content: podfileLockAfter,
+          path: podfileLockPath,
+          sha: podfileLockBlob.data.sha,
+          type: "blob",
+          mode: "100644",
+        },
+      ],
+    });
+    await octokit.git.createCommit({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      committer: {
+        email: "hi@jonny.io",
+        name: "jonnybot",
+      },
+      message: "🤖 Fixed your fucking Podfile",
+      tree: commit.data.sha,
+      parents: [curentRef.data.object.sha],
+    });
+    await octokit.git.updateRef({
+      repo: repo,
+      owner: owner,
+      ref,
+      sha: curentRef.data.object.sha,
+    });
+    console.log(
+      "Fixed the fucking Podfile. Failing this commit now, wait for the next one!"
+    );
+    throw new Error("Podfile is not up to date (commit fix was made)");
+  } else {
+    console.log("Pod file is up to date!");
+  }
+});

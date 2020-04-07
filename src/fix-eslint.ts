01f1fbba6327de791179719d6227582d3568a4b3
Jonny Burger <jonathanburger11@gmail.com>
2020-04-07 15:23:19 +0200

    Add Action

--- src/fix-eslint.ts
+++ src/fix-eslint.ts
@@ -57,7 +57,7 @@ export const fixEslint = async (): Promise<void> => {
 		.filter((f) => f.includes('modified:'));
 	const modifiedFiles = modifiedRaw
 		.map((m) => m.replace('modified:', '').trim())
-		.filter((f) => f !== 'package-lock.json' && f.includes('Podfile.lock'))
+		.filter((f) => f !== 'package-lock.json' && !f.includes('Podfile.lock'))
 		// Fix a maximum of 20 files
 		.slice(0, 20);
 	console.log('modified files:');
